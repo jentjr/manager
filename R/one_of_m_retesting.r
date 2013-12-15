71aaa0544950b5cc57f053e629dee00a9ddf0953
Justin Jent <jentjr@gmail.com>
2013-12-15 00:51:28 -0500

    formatted one_of_m_retesting.r and added list for output

--- R/one_of_m_retesting.r
+++ R/one_of_m_retesting.r
@@ -23,6 +23,7 @@
 # use ERPC power values as the reference power
 
 calc_kappa <- function(n, w, coc, ne, m, ord = 1, swfpr = 0.1){
+  
   # user supplied values of n, w, coc, df, evaluation frequency, m, and ord
   n <- n
   w <- w
@@ -33,7 +34,9 @@ calc_kappa <- function(n, w, coc, ne, m, ord = 1, swfpr = 0.1){
   ord <- ord
   swfpr <- swfpr
   alph <- 1 - (1 - swfpr) ^ (1 / (coc * w))
+  
   ref= c()
+  
   if (ne == 1) ref = c(0.54, 0.81)
   if (ne == 2) ref = c(0.59, 0.85)
   if (ne == 4) ref = c(0.60, 0.86)
@@ -48,34 +51,59 @@ calc_kappa <- function(n, w, coc, ne, m, ord = 1, swfpr = 0.1){
   
   # recursive function to compute correct multiplier within limits (lo, hi)
   kfind <- function(lo, hi, n, alph, ne, tol) {
+    
     if (abs(hi - lo) < tol2) return(lo)
+    
     nc <- function(x) sqrt(n) * qnorm(x) / sqrt(ord)
+    
     tt <- sqrt(n) * lo
+    
     g <- function(x) ne * m * (1 - (1 - x) ^ m) ^ (ne - 1) * (1 - x) ^ (m - 1) * pt(tt, df, nc(x))
+    
     klo <- 1 - alph - integrate(g, 0, 1)$value
+    
     if (abs(klo) <= tol) return(lo)
+    
     tt <- sqrt(n) * hi
+    
     khi <- 1 - alph - integrate(g, 0, 1)$value
+    
     if (abs(khi) <= tol) return(hi)
+    
     tt <- sqrt(n) * (mean(c(lo, hi)))
+    
     kmid <- 1 - alph - integrate(g, 0, 1)$value
-    if (abs(kmid) <= tol) return(mean(c(lo, hi)))
+    
+    if (abs(kmid) <= tol) {
+      return(mean(c(lo, hi)))
+    }
+    
     if (sign(klo) != sign(khi)) {
       if (sign(klo) != sign(kmid)) {
-        kfind(lo, mean(c(lo, hi)), n, alph, ne, tol) }
+        kfind(lo, mean(c(lo, hi)), n, alph, ne, tol) 
+      }
       else {
-        kfind(mean(c(lo, hi)), hi, n, alph, ne, tol) } }
+        kfind(mean(c(lo, hi)), hi, n, alph, ne, tol) } 
+    }    
     else {
-      stop('bad limits') }
+      stop('bad limits') 
+    }
   }
+  
   del <- c(3, 4)
   pow <- c()
   powrate <- c()
   kap <- kfind(ll, ul, n, alph, ne, tol)
+  
   for (jj in 1:length(del)) {
+    
     dc <- del[jj]
     tt <- sqrt(n) * kap
-    nc <- function(x) {sqrt(n) * (qnorm(x) / sqrt(ord) + del[jj])}
+    
+    nc <- function(x) {
+      sqrt(n) * (qnorm(x) / sqrt(ord) + del[jj])
+    }
+    
     h <- function(x) {
       if (ne == 1) {
         m * ((1 - x) ^ (m - 1)) * pt(tt, df, nc(x))
@@ -86,22 +114,23 @@ calc_kappa <- function(n, w, coc, ne, m, ord = 1, swfpr = 0.1){
     }
     pow[jj] <- 1 - integrate(h, 0, 1, stop.on.error = F)$value
   }
+  
   if ((pow[1] >= ref[1]) && (pow[2] >= ref[2])) powrate= 'GOOD'
   if ((pow[1] < ref[1]) && (pow[2] >= ref[2])) powrate= 'ACCEPTABLE'
   if ((pow[1] >= ref[1]) && (pow[2] < ref[2])) powrate= 'ACCEPTABLE'
   if ((pow[1] < ref[1]) && (pow[2] < ref[2])) powrate= 'LOW'
   
   
-  print(paste('intrawell 1-of-m'), quote=F)
-  print(paste('n =', n, ';', 'w =', w, ";", 'coc =', coc,";", 'ne =', ne), quote=F)
-  print(paste('m =', m, ";", 'ord =', ord), quote=F)
-  print(paste('ref power from ERPC at 3 and 4 SDs'), quote=F)
-  print(ref)
-  print(paste('kappa =', round(kap, 2)), quote=F)
-  print(paste('calculated power at 3 and 4 SDs'), quote=F)
-  print(round(pow, 3))
-  print(paste('power rating = ', powrate), quote=F)
+  list("background sample size" = n,
+       "number of wells" = w,
+       "number of constituents" = coc,
+       "sampling frequncy" = ne,
+       "type of 1-of-m retesting scheme" = m,
+       "order of the mean to be predicted" = ord,
+       "kappa" = kap,
+       "ref power from ERPC at 3 and 4 SDs" = ref,
+       "calculated power at 3 and 4 SDs" = pow,
+       "power rating" = powrate)
   
-  return(kap)
 }
 
