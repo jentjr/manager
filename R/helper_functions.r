2fd68d05d0047bffbc80050041f6d66f8827203c
Justin Jent <jentjr@gmail.com>
2013-12-16 16:12:51 -0500

    added remove_dup function and fixed DESCRIPTION file

--- R/helper_functions.r
+++ R/helper_functions.r
@@ -12,6 +12,19 @@ percent_lt <- function(lt) {
   return(p)
 }
 
+
+#' function to remove duplicate well names
+#'
+#'@param df data frame of groundwater data in long format with location_id as the column 
+#' name for monitoring wells
+#' 
+
+remove_dup <- function(df){
+  df_nodup <- df[-grep("*Dup", df$location_id), ]
+  return(df_nodup)
+}
+
+
 #' Function to summarize the number of samples, mean, sd, and percentage of 
 #' non-detects. This is useful for calculating the upper prediction limit.
 #' 
