da3369d9624f84db7d9ed1c4bf4e9daa229fed2a
Justin Jent <jentjr@gmail.com>
2013-12-18 22:25:07 -0500

    added switch feature to upload file types

--- inst/app/ui.r
+++ inst/app/ui.r
@@ -1,48 +1,36 @@
 # Define layout of interface
 shinyUI(pageWithSidebar(
-  
   headerPanel(title = "", windowTitle = "Groundwater Monitoring"),
-  
     sidebarPanel(
-      
       tags$head(
         tags$style(type="text/css", "select { width: 200px; }"),
-        
-        tags$style(type="text/css", "textarea { max-width: 200px; }"),
-        
-        tags$style(type="text/css", ".jslider { max-width: 200px; }"),
-        
-        tags$style(type='text/css', ".well { max-width: 230px; }"),
-        
+        tags$style(type="text/css", "textarea { max-width: 400px; }"),
+        tags$style(type="text/css", ".jslider { max-width: 400px; }"),
+        tags$style(type='text/css', ".well { max-width: 430px; }"),
         tags$style(type='text/css', ".span4 { max-width: 270px; }")
         ),
-      
-      
       fileInput(inputId = "manages_path",
-                label = "Enter path to MANAGES database",
-                accept = '.mdb'),
-      
+                label = "Enter path to MANAGES database\n
+                .csv file, or excel",
+                accept = c(".mdb", ".csv", ".xls")),
+      conditionalPanel(
+        condition = "input.data == FALSE",
+        radioButtons(inputId = "file_type", 
+                     label = "File Extension", 
+                     choices = c(".mdb", ".csv", ".xls"))),
       conditionalPanel(
         condition = "input.data == TRUE",
         uiOutput("wells")),
-
       uiOutput("analytes"),
-      
       helpText("CTRL- Click to select multiple wells and constituents."),
-      
       conditionalPanel(
         condition = "input.data == TRUE",
         uiOutput("date_ranges")),
-      
-      
       checkboxInput(inputId = "scale_plot",
                     label = "Scale Free Plot"),
-      
       checkboxInput(inputId = "date_lines",
                     label = "Show Date Ranges")
-      
       ),
-
   mainPanel(
     tabsetPanel(
       tabPanel("Data Table",htmlOutput("well_table")),
@@ -54,6 +42,4 @@ shinyUI(pageWithSidebar(
       tabPanel("Map", plotOutput("well_map"))
       )
     )
-
 ))
-        
