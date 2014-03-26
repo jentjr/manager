bbfd72a98e2d53c9839b00887d6477a50361f830
Justin Jent <jentjr@gmail.com>
2014-03-25 23:06:46 -0400

    added heatmap for mcl comparison

--- R/compare_mcl.r
+++ R/compare_mcl.r
@@ -6,33 +6,39 @@ compare_MCL <- function(df) {
 }
   
 
-parse_mcl <- function(x = "param_name") {
-  x <- as.quoted(x)
-  df[,x] <- grepl("Arsenic", df[,x])
-  df[,x] <- grepl("Antimony", df[,x])
-  df[,x] <- grepl("Barium", df[,x])
-  df[,x] <- grepl("Beryllium", df[,x])
-  df[,x] <- grepl("Cadmium", df[,x])
-  df[,x] <- grepl("Chromium", df[,x])
-  df[,x] <- grepl("Mercury", df[,x])
-  df[,x] <- grepl("Selenium", df[,x])
-  df[,x] <- grepl("Thallium", df[,x])
-  df[,x] <- grepl("Uranium", df[,x])
-  df[,x] <- grepl("Nitrite", df[,x])
-  df[,x] <- grepl("Nitrate", df[,x])
-  df[,x] <- grepl("Zinc", df[,x])
-  df[,x] <- grepl("Aluminum", df[,x])
-  df[,x] <- grepl("Iron", df[,x])
-  df[,x] <- grepl("Manganese", df[,x])
-  df[,x] <- grepl("Chloride", df[,x])
-  df[,x] <- grepl("Fluoride", df[,x])
-  df[,x] <- grepl("Total Dissolved Solids", df[,x])
-  df[,x] <- grepl("TDS", df[,x])
-  df[,x] <- grepl("Sulfate", df[,x])
-  df[,x] <- grepl("pH", df[,x])
-  df[,x] <- grepl("Copper", df[,x])
-  df[,x] <- grepl("Lead", df[,x])
-  return(df)
+parse_mcl <- function(df) {
+  element <- mcl$param_name[1]
+  tmp <- gw_data[grepl(paste(element), gw_data$param_name)==1,]
+  tmp$exceedances <- ifelse(tmp$analysis_result > mcl$upper_limit[1] | 
+                              tmp$analysis_result < mcl$lower_limit[1], 1, 0)
+  ddply(tmp, .(location_id), summarise, total_exceed = sum(exceedances))
 }
 
 
+# for each param in mcl
+#   match to data param
+#   compare mcl to results
+#     if greater or less than limit exceedance = 1
+#   count exceedances and summarize (1 row)
+#   combine rows
+# return new data frame
+
+# Spacetime plot for each mcl with location on y axis, time on x axis
+#  solid color if exceeds mcl
+gw_heatmap <- function(df){
+  tmp <- dcast(df, value.var = "analysis_result", location_id ~ sample_date)
+  row.names(tmp) <- tmp$location_id
+  tmp <- tmp[, -1]
+  tmp <- data.matrix(tmp)
+  heatmap(tmp, Rowv=NA, Colv=NA, main = df$param_name[1])
+}
+
+gw_heatmap_multi <- function(df) {
+  d_ply(df, .(param_name), gw_heatmap, .print=TRUE)
+}
+
+# # example exceedance table output
+# 
+# location_id | arsenic | antimony | boron 
+# ----------- | ------- | -------- | ----- 
+# MW-1        | 10      |   1      |  5
