b2b0ae7ed9ccf96ac8e65f456305d224b4c3f664
jentjr <jentjr@gmail.com>
2014-08-19 14:24:50 -0400

    Update ui.R

--- inst/app/groundwater_stats/ui.R
+++ inst/app/groundwater_stats/ui.R
@@ -1,78 +1,127 @@
 library(ggvis)
 
-shinyUI(navbarPage("Groundwater Monitoring Statistics",
-  tabPanel("Data",
-    sidebarLayout(
-      sidebarPanel(
-        fileInput(inputId = "manages_path", 
-                  label = "Browse to MANAGES Site.mdb file",
-                  accept = c(".mdb", ".csv", ".xls")),
-          conditionalPanel(
-            condition = "input.data == FALSE",
-              radioButtons(inputId = "file_type", 
-                           label = "File Extension", 
-                           choices = c(".mdb", ".csv", ".xls")))
-      ),
-      mainPanel(
-        dataTableOutput("well_table")
-      )
-    )          
-  ),
-  navbarMenu("Plots",
-    tabPanel("Boxplots",
-      sidebarLayout(
-       sidebarPanel(
-         uiOutput("wells_box"),
-         uiOutput("analytes_box"),
-         uiOutput("date_ranges_box"),
-         checkboxInput("scale_plot_box", "Scale Free Plot"),
-         checkboxInput("coord_flip_box", "Flip Coordinates")
-        ),
-        mainPanel(
-          plotOutput("box_plot", height = 675, width = 825)
-        )
-      )       
-    ),
-    tabPanel("Time series",
+shinyUI(navbarPage("GWSTATS",
+  tabPanel("Data Input",
       sidebarLayout(
         sidebarPanel(
-          uiOutput("wells_time"),
-          uiOutput("analytes_time"),
-          uiOutput("date_ranges_time"),
-          checkboxInput("short_name", "Abbreviate Constituent Name"),
-          checkboxInput("date_lines", "Show Date Ranges")
+          fileInput(inputId = "manages_path", 
+                    label = "Browse to MANAGES Site.mdb file",
+                    accept = c(".mdb", ".csv", ".xls")),
+            conditionalPanel(
+              condition = "input.data == FALSE",
+                radioButtons(inputId = "file_type", 
+                             label = "File Extension", 
+                             choices = c(".mdb", ".csv", ".xls")))
         ),
         mainPanel(
-          plotOutput("time_plot", height = 675, width = 825)
+          dataTableOutput("well_table")
+        )
+      )          
+    ),
+  tabPanel("Data Summary",
+    sidebarLayout(
+      sidebarPanel(
+       uiOutput("summary_date_ranges")  
+      ),
+      mainPanel(
+        dataTableOutput("summary_table")  
+      )
+    )         
+  ),  
+  navbarMenu("Time Series",
+    tabPanel("By Well",
+      sidebarLayout(
+        sidebarPanel(
+          uiOutput("wells_time_well"),
+          uiOutput("analytes_time_well"),
+          uiOutput("date_ranges_time_well"),
+          checkboxInput("short_name_well", "Abbreviate Constituent Name"),
+          checkboxInput("date_lines_well", "Show Date Ranges")
+        ),
+        mainPanel(
+          plotOutput("ts_by_well", height = 675, width = 825)
         )
       )       
     ),
-    tabPanel("Piper Diagram",
-     sidebarLayout(
-      sidebarPanel(
-       uiOutput("wells_piper"),
-       uiOutput("date_ranges_piper"),
-       textInput(inputId = "Mg", label = "Mg", value = "Magnesium, dissolved"),
-       textInput(inputId = "Ca", label = "Ca", value = "Calcium, dissolved"),
-       textInput(inputId = "K", label = "K", value = "Potassium, dissolved"),
-       textInput(inputId = "Na", label = "Na", value = "Sodium, dissolved"),
-       textInput(inputId = "Cl", label = "Cl", value = "Chloride, total"),
-       textInput(inputId = "SO4", label = "SO4", value = "Sulfate, total"),
-       textInput(inputId = "Alk", label = "Alkalinity", 
-                 value = "Alkalinity, total (lab)"),
-       textInput(inputId = "TDS", label = "TDS", 
-                 value = "Total Dissolved Solids"),
-       checkboxInput(inputId = "TDS_plot",
-                     label = "Scale by Total Dissolved Solids")
-      ),
-      mainPanel(
-       plotOutput("piper_plot", height = 700, width = 800)  
-      )
-     )
+    tabPanel("By Constituent",
+      sidebarLayout(
+        sidebarPanel(
+          uiOutput("wells_time_const"),
+          uiOutput("analytes_time_const"),
+          uiOutput("date_ranges_time_const"),
+          checkboxInput("short_name_const", "Abbreviate Constituent Name"),
+          checkboxInput("date_lines_const", "Show Date Ranges")
+        ),
+        mainPanel(
+          plotOutput("ts_by_const", height = 675, width = 825)  
+        )
+      )         
     )
   ),
-  navbarMenu("Statistics",
-    tabPanel("Prediction Limit",
+  navbarMenu("Boxplots",
+    tabPanel("By Well",
+      sidebarLayout(
+        sidebarPanel(
+          uiOutput("wells_box_well"),
+          uiOutput("analytes_box_well"),
+          uiOutput("date_ranges_box_well"),
+          checkboxInput("short_name_box_well", "Abbreviate Constituent Name"),
+          checkboxInput("coord_flip_box_well", "Flip Coordinates")
+        ),
+        mainPanel(
+          plotOutput("box_plot_well", height = 675, width = 825)  
+        )
+      )         
+    ),
+    tabPanel("By Constituent",
+      sidebarLayout(
+        sidebarPanel(
+          uiOutput("wells_box_const"),
+          uiOutput("analytes_box_const"),
+          uiOutput("date_ranges_box_const"),
+          checkboxInput("short_name_box_const", "Abbreviate Constituent Name"),
+          checkboxInput("coord_flip_box_const", "Flip Coordinates")
+        ),
+        mainPanel(
+          plotOutput("box_plot_const", height = 675, width = 825)
+        )
+      )       
+    )
+  ),
+  navbarMenu("Geochemical Plots",
+    tabPanel("Piper Diagram",
+      sidebarLayout(
+        sidebarPanel(
+          uiOutput("wells_piper"),
+          uiOutput("date_ranges_piper"),
+          textInput(inputId = "Mg", label = "Mg", 
+                    value = "Magnesium, dissolved"),
+          textInput(inputId = "Ca", label = "Ca", 
+                    value = "Calcium, dissolved"),
+          textInput(inputId = "K", label = "K", 
+                    value = "Potassium, dissolved"),
+          textInput(inputId = "Na", label = "Na", 
+                    value = "Sodium, dissolved"),
+          textInput(inputId = "Cl", label = "Cl", 
+                    value = "Chloride, total"),
+          textInput(inputId = "SO4", label = "SO4", 
+                    value = "Sulfate, total"),
+          textInput(inputId = "Alk", label = "Alkalinity", 
+                    value = "Alkalinity, total (lab)"),
+          textInput(inputId = "TDS", label = "TDS", 
+                    value = "Total Dissolved Solids"),
+          checkboxInput(inputId = "TDS_plot",
+                        label = "Scale by Total Dissolved Solids")
+        ),
+        mainPanel(
+          plotOutput("piper_plot", height = 700, width = 800)  
+        )
+      )
+    ),
+    tabPanel("Stiff Diagram")
+  ),
+  navbarMenu("Prediction Intervals",
+    tabPanel("Intra-well",
       sidebarLayout(
        sidebarPanel(
         uiOutput("wells_upl"),
@@ -104,6 +153,6 @@ shinyUI(navbarPage("Groundwater Monitoring Statistics",
        )
       )       
     ),
-    tabPanel("Tolerance Interval")
-  )
+    tabPanel("Inter-well")
+  ) 
 ))
