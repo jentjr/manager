327ab97fb38765e16ea74d6f13909a299b9628cc
Justin Jent <jentjr@gmail.com>
2016-02-02 23:19:26 -0500

    start on database creation

---
+++ R/database.R
@@ -0,0 +1,23 @@
+library(DBI)
+
+new_database <- function(path) {
+  
+  db <- dbConnect(RSQLite::SQLite(), path) 
+  
+  dbSendQuery(db, "CREATE TABLE IF NOT EXISTS wellConstruction(boringID TEXT, 
+            wellID TEXT, longitude TEXT, latitude TEXT, bottomScreen REAL, 
+            topScreen REAL, topRiser REAL)")
+  
+  dbSendQuery(db, "CREATE TABLE IF NOT EXISTS sampleData(wellID TEXT, 
+              sampleDate TEXT, method TEXT, analyte TEXT, censored BOOLEAN,
+              detectionLimit REAL, reportingLimit REAL, result REAL, 
+              units TEXT, comments TEXT)")
+  
+  dbDisconnect(db)
+  
+  return(db)
+  
+}
+
+dbSendQuery(db, "INSERT INTO wellConstruction VALUES('MW-1', 'MW-1', '45.23', 
+            '80.45', 128.76, 100.56, 56.10)")
