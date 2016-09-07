4bbf1b7ec3cf184be6c0ac8e6f35374dcae5a85b
Justin Jent <jentjr@gmail.com>
2016-09-07 13:54:56 -0400

    clean up event summary and export

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -28,19 +28,42 @@ percent_gt <- function(gt) {
 #' Example: If you have wells named MW-1 and another named MW-1 Duplicate
 #' this function will remove the MW-1 Duplicate sample
 #'
-#'@param df data frame of groundwater data in long format with location_id as the column 
-#' name for monitoring wells
+#' @param df groundwater data frame
 #' @export
 
 remove_dup <- function(df){
+  
   df_nodup <- df[-grep("*Dup", df$location_id), ]
+  
   return(df_nodup)
+  
+}
+
+include_dup <- function(df, wells) {
+  
+  pattern <- paste(wells, "[:space:]*Dup")
+  
+  dups <- unique(df[grep(pattern, df$location_id), ]$location_id)
+  
+  dups <- as.character(droplevels(dups))
+  
+  wells <- append(wells, dups)
+  
+  df_dup <- df %>%
+    filter(location_id %in% wells)
+  
+  df_dup <- droplevels(df_dup)
+  
+  return(df_dup)
+  
 }
 
 replace_missing <- function(df){
+  
   df$analysis_result <- ifelse(df$analysis_result == -999.9, NA, 
                                  df$analysis_result)
   return(df)
+  
 }
 
 #' Function to convert gwdata frame to censored data frame
@@ -86,9 +109,56 @@ lt_summary <- function(df, start_date, end_date){
   return(lt)
 }
 
+#' Function to join columns of lt_measure and sample results
+#' 
+#' @param df groundwater data frame
+#' @param col_name qouted column name for the result
+#' @export
+
+join_lt <- function(df, col_name) {
+  
+  .join_lt <- function() {
+    paste(df$lt_measure, df$analysis_result, sep = " ")
+  }
+  
+  df$result <- ifelse(df$lt_measure == "<" | df$lt_measure == ">", 
+                        .join_lt(), df$analysis_result)
+  
+  names(df)[names(df) == "result"] <- col_name
+  
+  return(df)
+  
+}
+
+#' Function to return a column of parameter name with units
+#' @param df groundwater data frame
+#' @param col_name quoted column name to return
+#' @export
+
+name_units <- function(df, col_name, short_name = TRUE) {
+  
+  if (short_name == TRUE) {
+    
+    df$param_unit <- paste0(df$short_name, " (", df$default_unit, ")")
+    
+  }
+  
+  else {
+    
+    df$param_unit <- paste0(df$param_name, " (", df$default_unit, ")")
+    
+  }
+ 
+  names(df)[names(df) == "param_unit"] <- col_name
+  
+  return(df)
+
+}
+
+
 #' Function to export data from manages to excel format required by OEPA
 #' 
-#' @param df data frame
+#' @param df groundwater data frame
 #' @param wells list of wells to be exported
 #' @param constituents list of constituents to be exported
 #' @param file full file path name with extension for export
@@ -99,14 +169,9 @@ export_OEPA <- function(df, wells, constituents, file, ...){
   df <- df %>% 
     dplyr::filter(param_name %in% constituents, location_id %in% wells)
   
-  join_lt <- function() {
-    paste(df$lt_measure, df$analysis_result, sep = " ")
-  }
-  
-  df$result <- ifelse(df$lt_measure == "<" | df$lt_measure == ">", 
-                      join_lt(), df$analysis_result)
+  df <- join_lt(df, "result")
  
-  df$param_unit <- paste0(df$param_name, " (",df$default_unit, ")")
+  df <- name_units(df, "param_unit")
   
   df <- df %>%
     dplyr::select(lab_id, location_id, sample_date, param_unit, result) %>%
@@ -120,41 +185,34 @@ export_OEPA <- function(df, wells, constituents, file, ...){
                         startRow = 1, startCol = 1, rowNames = FALSE)
   }
   
-  plyr::d_ply(df, .(location_id), oepa_cast)
+  plyr::d_ply(df, plyr::.(location_id), oepa_cast)
 
   openxlsx::saveWorkbook(wb, file = file, ...)
+
 }
+
 
 #' Function to export summary table for a sampling event
 #' 
-#' @param df data frame in the format location_id, sample_date, param_name,
-#'  lt_measure, default_unit
-#'  @param wells list of wells
-#'  @param constituents list of constituents
-#'  @param start start date
-#'  @param end end date 
-#' 
+#' @param df groundwater data frame
+#' @param start start date
+#' @param end end date 
+#' @export
 
-event_summary <- function(df, wells, constituents, start, end){
-  df <- df[df$location_id %in% wells &
-           df$param_name %in% constituents &
-           df$sample_date >= start &
-           df$sample_date <= end, ]
+event_summary <- function(df, start, end){
   
-  df$param_name <- paste(df$param_name, " (", df$default_unit, ")", sep = "")
+  df <- df %>% 
+    filter(sample_date >= start & sample_date <= end)
   
-  join_lt <- function() {
-    paste(df$lt_measure, df$analysis_result, sep = " ")
-  }
+  df <- name_units(df, "param_units")
   
-  df$result <- ifelse(df$lt_measure == "<" | df$lt_measure == ">", 
-                      join_lt(), df$analysis_result)
+  df <- join_lt(df, "result")
   
-  id <- dplyr::group_by(df, location_id, sample_date, param_name)
-  id <- dplyr::mutate(id, group = n())
-    
+  id <- dplyr::group_by(df, location_id, sample_date, param_units)
+
   out <- reshape2::dcast(id, value.var = "result", 
-                          group + location_id + sample_date ~ param_name, 
-                          margins = FALSE)[-1]
+                         location_id + sample_date ~ param_units, 
+                         margins = FALSE)
   return(out)
+  
 }
