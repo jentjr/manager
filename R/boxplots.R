f08128434f899d66d623ae81cd237b5371d82d6c
Justin Jent <jentjr@gmail.com>
2017-02-08 17:16:20 -0500

    clean up boxplots

--- R/boxplots.R
+++ R/boxplots.R
@@ -1,90 +1,91 @@
-#' Function to plot multiple boxplots for groundwater data 
+#' Function to plot boxplots for groundwater data 
 #' 
-#' @param df groundwater data
-#' @param facet_by parameter to group data by
-#' @param name If name = "short" the analyte will be abbreviated
+#' @param df groundwater data in tidy, long format
+#' @param x column to be used for the x axis
+#' @param y column to be used for the y axis
+#' @param fill column used to fill the variable
+#' @param short_name If TRUE the constituent name will be abbreviated
 #' @param flip_coords If TRUE the axes are flipped
+#' @param legend_title Legend title for fill variable
 #' @export
 
 boxplot <- function(df, 
-                    facet_by = "param_name",
+                    x = "location_id",
+                    y = "analysis_result",
+                    fill = NULL,
                     short_name = FALSE, 
-                    coord_flip = FALSE){
+                    coord_flip = FALSE,
+                    legend_title = NULL){
   
-  if (facet_by == "param_name") {
-    
     df %>% 
       group_by(param_name) %>%
-      do(plot = .boxplot(., 
-                         facet_by = "param_name",
-                         short_name = short_name,
-                         coord_flip = coord_flip))
-  }
-  
-  if (facet_by == "location_id") {
-    
-    df %>%
-      group_by(location_id) %>%
-      do(plot = .boxplot(., 
-                         facet_by = "location_id",
-                         short_name = short_name,
-                         coord_flip = coord_flip))
-  }
+      do(plot = .boxplot(.,
+                  x = x,
+                  y = y,
+                  fill = fill,
+                  short_name = short_name,
+                  coord_flip = coord_flip,
+                  legend_title = legend_title
+                )
+      )
+
 }
 
-#' Function to plot boxplots for groundwater data
-#' @param df groundwater data
-#' @param facet_by parameter to group data by
-#' @param short_name If TRUE, the analyte name will be abbreviated
+#' Helper function to plot boxplots for groundwater data 
+#' 
+#' @param df groundwater data in tidy, long format
+#' @param x column to be used for the x axis
+#' @param y column to be used for the y axis
+#' @param fill column used to fill the variable
+#' @param short_name If TRUE the constituent name will be abbreviated
 #' @param flip_coords If TRUE the axes are flipped
+#' @param legend_title Legend title for fill variable
 
 .boxplot <- function(df, 
-                     facet_by = "location_id", 
+                     x = "location_id", 
+                     y = "analysis_result", 
+                     fill = NULL,
                      short_name = FALSE, 
-                     coord_flip = FALSE){
+                     coord_flip = FALSE,
+                     legend_title = NULL) {
 
+  if (!is.null(fill) & is.null(legend_title)) {
+    
+    legend_title <- fill
+    
+  }
+  
   if (isTRUE(short_name)) {
+    
     df$name_units <- paste(df$short_name, " (", df$default_unit, ")", sep = "")
+    
   } else {
+    
     df$name_units <- paste(df$param_name, " (", df$default_unit, ")", sep = "")
+    
   }
-  if (facet_by == "param_name") {
-    b <- ggplot(df, aes_(x = quote(location_id), 
-                         y = quote(analysis_result), 
-                         fill = quote(gradient))) + 
-      theme_bw() + 
-      ylab(paste("Analysis Result"," (", df$default_unit[1], ")", sep = "")) + 
-      xlab("Location ID") +
-      # guides(fill = guide_legend("Location ID")) +
-      theme(legend.background = element_rect()) + 
-      theme(plot.margin = grid::unit(c(1, 1, 1, 1), "lines")) +
-      theme(axis.title.x = element_text(vjust = -0.5, size = 15)) +
-      theme(axis.text.x = element_text(angle = 90, size = 13)) +
-      theme(axis.title.y = element_text(vjust = 0.5, size = 15)) +
-      theme(axis.text.y = element_text(size = 13)) +
-      theme(plot.title = element_text(hjust = 0.5)) +
-      geom_boxplot() + 
-      ggtitle(paste("Boxplot for", df$name_units, "\n", sep = " "))
-  }
-  if (facet_by == "location_id") {
-    b <- ggplot(df, aes_(x = quote(name_units), 
-                         y = quote(analysis_result))) + 
-      theme_bw() + 
-      ylab("Analysis Result") + 
-      xlab("Constituent") +
-      # guides(fill = guide_legend("Constituent")) +
-      theme(legend.background = element_rect()) + 
-      theme(plot.margin = grid::unit(c(1, 1, 1, 1), "lines")) +
-      theme(axis.title.x = element_text(vjust = -0.5, size = 15)) +
-      theme(axis.text.x = element_text(angle = 90, size = 13)) +
-      theme(axis.title.y = element_text(vjust = 0.5, size = 15)) +
-      theme(axis.text.y = element_text(size = 13)) +
-      theme(plot.title = element_text(hjust = 0.5)) +
-      geom_boxplot() + 
-      ggtitle(paste("Boxplot for", df$location_id, "\n", sep = " "))
-  }
+  
+  b <- ggplot(df, aes_string(x = x, y = y, fill = fill)) + 
+    theme_bw() + 
+    ylab(paste("Analysis Result"," (", df$default_unit[1], ")", sep = "")) + 
+    xlab("Location ID") +
+    guides(fill = guide_legend(paste0(legend_title))) +
+    theme(legend.background = element_rect()) + 
+    theme(plot.margin = grid::unit(c(1, 1, 1, 1), "lines")) +
+    theme(axis.title.x = element_text(vjust = -0.5, size = 15)) +
+    theme(axis.text.x = element_text(angle = 90, size = 13)) +
+    theme(axis.title.y = element_text(vjust = 0.5, size = 15)) +
+    theme(axis.text.y = element_text(size = 13)) +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    geom_boxplot() + 
+    ggtitle(paste("Boxplot for", df$name_units, "\n", sep = " "))
+  
   if (isTRUE(coord_flip)) {
+    
     b <- b + coord_flip()
-  }      
+    
+  }
+  
   print(b)
+  
 }
