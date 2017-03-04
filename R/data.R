83ff14b89d156214c7661fa9037514b26d6cfbc2
Justin Jent <jentjr@gmail.com>
2017-03-03 23:43:28 -0500

    document data

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Groundwater quality data for the state of Ohio
+#'
+#' A dataset containing groundwater quality data for the state of Ohio.
+#'
+#' @format A data frame with 213845 rows and 47 variables:
+#' \describe{
+#'   \item{well_id}{well_id, Site Identifier}
+#'   \item{result_value}{analytical result}
+#'   ...
+#' }
+#' @source \url{http://epa.ohio.gov/ddagw/gwqcp.aspx}
+"ohio_data"
+
+#' Groundwater quality data for the state of Indiana
+#'
+#' A dataset containing groundwater quality data for the state of Indiana.
+#'
+#' @format A data frame with 162923 rows and 11 variables:
+#' \describe{
+#'   \item{SiteId}{Site Identifier}
+#'   \item{Result}{analytical result}
+#'   ...
+#' }
+#' @source \url{http://in.gov/idem/cleanwater/2453.html}
+"indiana_data"
