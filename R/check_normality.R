a397973bc397a0f3740e16f8eb91aaa381a368ba
Justin Jent <jentjr@gmail.com>
2017-10-22 03:24:34 -0400

    docs

--- R/check_normality.R
+++ R/check_normality.R
@@ -1,8 +1,18 @@
-#' Function to check if groundwater data has a Normal distribution
+#' Function to return either normal, lognormal, or non-parametric multiple 
+#' groundwater data locations and parameters.
 #' 
-#' @param x column of analysis result for groundwater data in tidy format
+#' @param df groundwater data frame in tidy format
 #' @param p p-value for gofTest
 #' @export
+
+est_dist <- function(df, p = 0.01) {  
+  df %>%
+    group_by(location_id, param_name) %>%
+    mutate(distribution = dist(analysis_result, p = p)) %>%
+    ungroup()
+}
+
+#' Helper function to check if data has a Normal distribution
 
 is_normal <- function(x, p = 0.01) {
   gof <- gofTest(x, dist = "norm")
@@ -14,11 +24,7 @@ is_normal <- function(x, p = 0.01) {
   }
 }
 
-#' Function to check if groundwater data has a Lognormal distribution
-#' 
-#' @param x column of analysis result for groundwater data in tidy format
-#' @param p p-value for gofTest
-#' @export
+#' Helper function to check if data has a Lognormal distribution
 
 is_lognormal <- function(x, p = 0.01) {
   lgof <- gofTest(x, dist = "lnorm")
@@ -30,11 +36,7 @@ is_lognormal <- function(x, p = 0.01) {
   }
 }
 
-#' Function to return distribution based on using p-value 
-#' 
-#' @param x column of analysis result for groundwater data in tidy format
-#' @param p p-value for gofTest
-#' @export
+#'Helper function to return distribution based on using p-value 
 
 dist <- function(x, p = 0.01) {
   n <- is_normal(x, p = p)
@@ -49,18 +51,4 @@ dist <- function(x, p = 0.01) {
       return("none")
     }
   } 
-}
-
-#' Function to return either normal, lognormal, or non-parametric multiple 
-#' groundwater data locations and parameters.
-#' 
-#' @param df groundwater data frame in tidy format
-#' @param p p-value for gofTest
-#' @export
-
-est_dist <- function(df, p = 0.01) {  
-  df %>%
-    group_by(LOCATION_ID, PARAM_NAME) %>%
-    mutate(distribution = dist(ANALYSIS_RESULT, p = p)) %>%
-    ungroup()
 }
