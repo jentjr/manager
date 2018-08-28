8de32d88003a8f44d51608ac335216b56c257b68
Justin R Jent <s225642@corp.aepsc.com>
2018-08-28 17:54:07 -0400

    begin work on spatial plots

--- R/spatial.R
+++ R/spatial.R
@@ -19,3 +19,62 @@ to_spatial <- function(df, crs) {
   return(out)
 }
 
+sp_plot <- function(df, 
+                    x = "analysis_result",
+                    color = "location_id",
+                    sample_interval = "semi-annually",
+                    group_var = "param_name", 
+                    facet_var = "sample_date") {
+  
+  df %>%
+    group_by_(group_var) %>%
+    do(plot = .sp_plot(., 
+                       x = x,
+                       color = color,
+                       sample_interval = sample_interval,
+                       group_var = group_var,
+                       facet_var = facet_var
+                       )
+       )
+       
+}
+
+#' Helper function for sp_plot
+
+.sp_plot <- function(df, 
+                     x = "analysis_result",
+                     color = "location_id",
+                     sample_interval = "semi-annually",
+                     group_var = NULL,
+                     facet_var = NULL) {
+  
+  if (sample_interval == "semi-annually") { 
+    
+      df$sample_date <- lubridate::semester(df$sample_date, with_year = TRUE)
+    
+    }
+  
+  if (sample_interval == "quarterly") { 
+    
+    df$sample_date <- lubridate::quarter(df$sample_date, with_year = TRUE)
+    
+  }
+  
+  p <- ggplot(data = df, aes_string(size = x, color = color)) + 
+    geom_sf() + 
+    theme_bw() + 
+    theme(plot.title = element_text(hjust = 0.5)) +
+    scale_size_continuous(name = "Analysis Result") + 
+    scale_color_viridis_d("Location ID")
+  
+  if (!is.null(facet_var)) {
+    
+    p <- p + facet_wrap(paste(facet_var)) +
+      ggtitle(paste("Plot for",
+                    df[[paste(group_var)]][1], "\n", sep = " "))
+    
+  }
+  
+  print(p)
+  
+}
