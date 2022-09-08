2a8d32fc1d1aed805f04aae7e96d61ab74653ed8
Justin R. Jent <jentjr@gmail.com>
2022-09-08 11:03:28 -0400

    Added page setup and style for write_excel

--- R/export.R
+++ R/export.R
@@ -6,12 +6,13 @@
 #' @param constituents list of constituents to be exported
 #' @param file full file path name with extension for export
 #' @param short_name TRUE/FALSE to abbreviate constituent name
+#' @param lab_id TRUE/FALSE to include the lab id
 #' @param overwrite TRUE/FALSE overwrite existing spreadsheet if it exists
 #'
 #' @export
 
 write_excel <- function(df, wells, constituents, file, short_name = TRUE,
-                        overwrite = FALSE) {
+                        lab_id = FALSE, overwrite = FALSE) {
 
   if (!requireNamespace("openxlsx", quietly = TRUE)) {
     stop("openxlsx needed for this function to work. Please install it.",
@@ -31,17 +32,43 @@ write_excel <- function(df, wells, constituents, file, short_name = TRUE,
   df <- df %>%
     name_units(short_name = short_name)
 
-  df <- df %>%
-    dplyr::select(location_id, lab_id, sample_date, param_name, analysis_result) %>%
-    tidyr::spread(param_name, analysis_result) %>%
-    arrange(location_id, sample_date)
+  if (lab_id == TRUE){
+    df <- df %>%
+      dplyr::select(location_id, lab_id, sample_date, param_name, analysis_result) %>%
+      tidyr::spread(param_name, analysis_result) %>%
+      arrange(location_id, sample_date)%>%
+      rename(`Location ID` = location_id, `Lab ID` = lab_id,
+             `Sample Date` = sample_date)
+  } else {
+    df <- df %>%
+      dplyr::select(location_id, sample_date, param_name, analysis_result) %>%
+      tidyr::spread(param_name, analysis_result) %>%
+      arrange(location_id, sample_date) %>%
+      rename(`Location ID` = location_id, `Sample Date` = sample_date)
+  }
 
+  hs <- openxlsx::createStyle(halign = "center", 
+                              valign = "center", 
+                              textDecoration = "Bold",
+                              border = "TopBottomLeftRight",
+                              wrapText = TRUE
+                              )
+  
+  df$`Sample Date` <- as.Date(df$`Sample Date`)
+  
   wb <- openxlsx::createWorkbook()
 
   data_cast <- function(x){
-    openxlsx::addWorksheet(wb, paste(x$location_id[1]))
-    openxlsx::writeData(wb, x, sheet = paste(x$location_id[1]),
-                        startRow = 1, startCol = 1, rowNames = FALSE)
+    openxlsx::addWorksheet(wb, paste(x$`Location ID`[1]))
+    openxlsx::pageSetup(wb, sheet = paste(x$`Location ID`[1]), 
+                        orientation = "landscape",
+                        paperSize = 3,
+                        fitToWidth = TRUE,
+                        fitToHeight = TRUE)
+    openxlsx::writeData(wb, x, sheet = paste(x$`Location ID`[1]),
+                        startRow = 1, startCol = 1, rowNames = FALSE,
+                        borders = "all",
+                        headerStyle = hs)
   }
 
   if (!requireNamespace("plyr", quietly = TRUE)) {
@@ -49,7 +76,7 @@ write_excel <- function(df, wells, constituents, file, short_name = TRUE,
          call. = FALSE)
   }
 
-  plyr::d_ply(df, plyr::.(location_id), data_cast)
+  plyr::d_ply(df, plyr::.(`Location ID`), data_cast)
 
   openxlsx::saveWorkbook(wb, file = file, overwrite = overwrite)
 
